93cc470aa4d876b1797ba5e1d3b5778f60289951
Eamon Nerbonne <eamon@nerbonne.org>
2010-12-07 13:40:40 +0100

    Added PAssert clone; to ease transition to/from Power Assert.NET use same syntax.

---
+++ ExpressionToCodeLib/PAssert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+
+namespace ExpressionToCodeLib {
+	public static class PAssert {
+		public static void IsTrue(Expression<Func<bool>> assertion) {
+			That(assertion,"PAssert.IsTrue failed for:");
+		}
+		public static void That(Expression<Func<bool>> assertion, string msg = null) {
+			if (!assertion.Compile()())
+				throw new PAssertFailedException((msg ?? "PAssert.That failed for:") + "\n" + ExpressionToCode.AnnotatedToCode(assertion.Body));
+		}
+	}
+}
---
+++ ExpressionToCodeTest/PAssertTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using ExpressionToCodeLib;
+
+namespace ExpressionToCodeTest {
+	[TestFixture]
+	public class PAssertTest {
+		[Test]
+		public void TestBasicStalks() {
+			var exc = Assert.Throws<PAssertFailedException>(() => {
+				PAssert.IsTrue(() => TimeSpan.FromMilliseconds(10.0).CompareTo(TimeSpan.FromMinutes(1.0)) > 0);
+			});
+			var msgLines = exc.Message.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+			Assert.That(msgLines[0], Contains.Substring("failed"));
+			Assert.That(msgLines[1], Is.EqualTo(@"TimeSpan.FromMilliseconds(10.0).CompareTo(TimeSpan.FromMinutes(1.0)) > 0"));
+			Assert.That(string.Join("\n", msgLines.Skip(1).ToArray()), Is.EqualTo(@"
+TimeSpan.FromMilliseconds(10.0).CompareTo(TimeSpan.FromMinutes(1.0)) > 0
+                |                   |                   |            |
+                |                   |                   |            false
+                |                   |                   00:01:00
+                |                   -1
+                00:00:00.0100000
+".Replace("\r","").Trim()));
+		}
+	}
+}
