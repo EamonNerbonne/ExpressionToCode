739aaeba92cf0a231b34d712babc93270d21eb35
Eamon Nerbonne <eamon@nerbonne.org>
2010-12-09 13:40:08 +0100

    Added tests to check for exaggerated value reporting due to casts

--- ExpressionToCodeTest/PAssertTest.cs
+++ ExpressionToCodeTest/PAssertTest.cs
@@ -25,5 +25,31 @@ TimeSpan.FromMilliseconds(10.0).CompareTo(TimeSpan.FromMinutes(1.0)) > 0
                 00:00:00.0100000
 ".Replace("\r","").Trim()));
 		}
+
+		[Test]
+		public void NoValuesForBoringCasts() {
+			var exc = Assert.Throws<PAssertFailedException>(() => {
+				PAssert.IsTrue(() => Equals(3,4));
+			});
+			var msgLines = exc.Message.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+			Assert.That(msgLines[0], Contains.Substring("failed"));
+			Assert.That(msgLines[1], Contains.Substring("Equals"));
+			Assert.That(msgLines[2].Count( c=>c=='|') , Is.EqualTo(1));
+		}
+
+		[Test]
+		public void ValuesForNonBoringCasts() {
+			ulong x = ulong.MaxValue;
+			var exc = Assert.Throws<PAssertFailedException>(() => 
+					PAssert.IsTrue(
+						() => 0 == (ulong)(uint)x
+					)
+				);
+			var msgLines = exc.Message.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+			Assert.That(msgLines[0], Contains.Substring("failed"));
+			Assert.That(msgLines[1], Is.EqualTo(@"ulong.MaxValue == (ulong)(uint)(ulong)x"));
+			Assert.That(msgLines[2].Count(c => c == '|'), Is.EqualTo(4));//for x, x+cast,x+cast+cast, ==, NOT for constant 0
+
+		}
 	}
 }
