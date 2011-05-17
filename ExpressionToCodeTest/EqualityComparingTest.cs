5d3557a6f7a45a4cfb4a1f223c062eeaac880251
Eamon Nerbonne <eamon@nerbonne.org>
2011-05-17 17:04:50 +0200

    More unit tests

--- ExpressionToCodeTest/EqualityComparingTest.cs
+++ ExpressionToCodeTest/EqualityComparingTest.cs
@@ -6,7 +6,10 @@ using NUnit.Framework;
 using ExpressionToCodeLib;
 
 #pragma warning disable 252,253
+// ReSharper disable NegativeEqualityExpression
+// ReSharper disable EqualExpressionComparison
 // ReSharper disable ConvertToConstant.Local
+// ReSharper disable RedundantCast
 namespace ExpressionToCodeTest {
 	[TestFixture]
 	public class EqualityComparingTest {
@@ -16,20 +19,26 @@ namespace ExpressionToCodeTest {
 		[Test]
 		public void EqualsOpDetected() {
 			Assert.AreEqual(EqualityExpressionClass.EqualsOp, EqualityExpressions.CheckForEquality(() => bla == bla2object));
+			Assert.AreEqual(EqualityExpressionClass.EqualsOp, EqualityExpressions.CheckForEquality(() => new DateTime(2011, 05, 17) == DateTime.Today));
 		}
 		[Test]
 		public void NotEqualsOpDetected() {
 			Assert.AreEqual(EqualityExpressionClass.NotEqualsOp, EqualityExpressions.CheckForEquality(() => bla != bla2object));
+			Assert.AreEqual(EqualityExpressionClass.NotEqualsOp, EqualityExpressions.CheckForEquality(() => new DateTime(2011, 05, 17) != DateTime.Today));
 		}
 		[Test]
 		public void EquatableDetected() { //TODO: makes interesting expressiontocode test too
 			Assert.AreEqual(EqualityExpressionClass.EquatableEquals, EqualityExpressions.CheckForEquality(() => ((IEquatable<string>)bla).Equals(bla2string)));
+			Assert.AreEqual(EqualityExpressionClass.EquatableEquals, EqualityExpressions.CheckForEquality(() => bla.Equals(bla2string)));
+			Assert.AreEqual(EqualityExpressionClass.EquatableEquals, EqualityExpressions.CheckForEquality(() => new DateTime(2011, 05, 17).Equals(DateTime.Today)));
 		}
 		[Test]
 		public void NoneDetected() {
 			Assert.AreEqual(EqualityExpressionClass.None, EqualityExpressions.CheckForEquality(() => bla.StartsWith("bla")));
+			Assert.AreEqual(EqualityExpressionClass.None, EqualityExpressions.CheckForEquality(() => !(bla == bla2object)));
+			Assert.AreEqual(EqualityExpressionClass.None, EqualityExpressions.CheckForEquality(() => DateTime.Equals(new DateTime(2011, 05, 17), DateTime.Today)));// no match since specific method
+			Assert.AreEqual(EqualityExpressionClass.None, EqualityExpressions.CheckForEquality(() => !ReferenceEquals(null, null)));
 		}
-
 		[Test]
 		public void ObjectEqualsDetected() {
 			Assert.AreEqual(EqualityExpressionClass.ObjectEquals, EqualityExpressions.CheckForEquality(() => bla2object.Equals(bla2object)));
@@ -38,15 +47,18 @@ namespace ExpressionToCodeTest {
 		}
 		[Test]
 		public void ObjectEqualsStaticDetected() {
-			Assert.AreEqual(EqualityExpressionClass.ObjectEqualsStatic, EqualityExpressions.CheckForEquality(() => object.Equals(bla,null)));
+			Assert.AreEqual(EqualityExpressionClass.ObjectEqualsStatic, EqualityExpressions.CheckForEquality(() => Equals(bla, null)));
+			Assert.AreEqual(EqualityExpressionClass.ObjectEqualsStatic, EqualityExpressions.CheckForEquality(() => Equals(null, 42)));
 		}
 		[Test]
 		public void ObjectReferenceEqualsDetected() {
-			Assert.AreEqual(EqualityExpressionClass.ObjectReferenceEquals, EqualityExpressions.CheckForEquality(() => object.ReferenceEquals(bla2object, bla2object)));
+			Assert.AreEqual(EqualityExpressionClass.ObjectReferenceEquals, EqualityExpressions.CheckForEquality(() => ReferenceEquals(bla2object, bla2object)));
+			Assert.AreEqual(EqualityExpressionClass.ObjectReferenceEquals, EqualityExpressions.CheckForEquality(() => ReferenceEquals(null, null)));
 		}
 		[Test]
 		public void SequenceEqualsDetected() {
-			Assert.AreEqual(EqualityExpressionClass.SequenceEqual, EqualityExpressions.CheckForEquality(() =>bla.AsEnumerable().SequenceEqual(bla2string) ));
+			Assert.AreEqual(EqualityExpressionClass.SequenceEqual, EqualityExpressions.CheckForEquality(() => bla.AsEnumerable().SequenceEqual(bla2string)));
+			Assert.AreEqual(EqualityExpressionClass.SequenceEqual, EqualityExpressions.CheckForEquality(() => new[]{'b','l','a'}.SequenceEqual(bla2string)));
 		}
 	}
 }
