39d183f1f168cee822705758db3c36af132bd00f
Eamon Nerbonne <eamon@nerbonne.org>
2011-05-17 16:49:14 +0200

    Added more tests for all non .NET 4.0 dependant equality types; IEquatable.Equals test still failing, next on the list...

--- ExpressionToCodeTest/EqualityComparingTest.cs
+++ ExpressionToCodeTest/EqualityComparingTest.cs
@@ -11,19 +11,42 @@ namespace ExpressionToCodeTest {
 	[TestFixture]
 	public class EqualityComparingTest {
 		static readonly string bla = "bla";
-		static readonly object bla2 = "bla2";
+		static readonly object bla2object = "bla2object";
 		static readonly string bla2string = "bla2string";
 		[Test]
 		public void EqualsOpDetected() {
-			Assert.AreEqual(EqualityExpressions.CheckForEquality(()=>bla == bla2), EqualityExpressionClass.EqualsOp);
+			Assert.AreEqual(EqualityExpressionClass.EqualsOp, EqualityExpressions.CheckForEquality(() => bla == bla2object));
 		}
 		[Test]
 		public void NotEqualsOpDetected() {
-			Assert.AreEqual(EqualityExpressions.CheckForEquality(() => bla != bla2), EqualityExpressionClass.NotEqualsOp);
+			Assert.AreEqual(EqualityExpressionClass.NotEqualsOp, EqualityExpressions.CheckForEquality(() => bla != bla2object));
 		}
 		[Test]
-		public void EquatableDetected() {
-			Assert.AreEqual(EqualityExpressions.CheckForEquality(() => ((IEquatable<string>)bla).Equals(bla2string) ), EqualityExpressionClass.EquatableEquals);
+		public void EquatableDetected() { //TODO: makes interesting expressiontocode test too
+			Assert.AreEqual(EqualityExpressionClass.EquatableEquals, EqualityExpressions.CheckForEquality(() => ((IEquatable<string>)bla).Equals(bla2string)));
+		}
+		[Test]
+		public void NoneDetected() {
+			Assert.AreEqual(EqualityExpressionClass.None, EqualityExpressions.CheckForEquality(() => bla.StartsWith("bla")));
+		}
+
+		[Test]
+		public void ObjectEqualsDetected() {
+			Assert.AreEqual(EqualityExpressionClass.ObjectEquals, EqualityExpressions.CheckForEquality(() => bla2object.Equals(bla2object)));
+			var anon = new { X = bla, Y = bla2object };
+			Assert.AreEqual(EqualityExpressionClass.ObjectEquals, EqualityExpressions.CheckForEquality(() => anon.Equals(anon)));
+		}
+		[Test]
+		public void ObjectEqualsStaticDetected() {
+			Assert.AreEqual(EqualityExpressionClass.ObjectEqualsStatic, EqualityExpressions.CheckForEquality(() => object.Equals(bla,null)));
+		}
+		[Test]
+		public void ObjectReferenceEqualsDetected() {
+			Assert.AreEqual(EqualityExpressionClass.ObjectReferenceEquals, EqualityExpressions.CheckForEquality(() => object.ReferenceEquals(bla2object, bla2object)));
+		}
+		[Test]
+		public void SequenceEqualsDetected() {
+			Assert.AreEqual(EqualityExpressionClass.SequenceEqual, EqualityExpressions.CheckForEquality(() =>bla.AsEnumerable().SequenceEqual(bla2string) ));
 		}
 	}
 }
