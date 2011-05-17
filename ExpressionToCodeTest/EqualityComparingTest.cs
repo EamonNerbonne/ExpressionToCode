f8e18511d9a897870974f80695602e4606f3f979
Eamon Nerbonne <eamon@nerbonne.org>
2011-05-17 16:27:21 +0200

    Added a few unit tests for equality comparison logic; Equatable test currently failing.

---
+++ ExpressionToCodeTest/EqualityComparingTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using ExpressionToCodeLib;
+
+#pragma warning disable 252,253
+// ReSharper disable ConvertToConstant.Local
+namespace ExpressionToCodeTest {
+	[TestFixture]
+	public class EqualityComparingTest {
+		static readonly string bla = "bla";
+		static readonly object bla2 = "bla2";
+		static readonly string bla2string = "bla2string";
+		[Test]
+		public void EqualsOpDetected() {
+			Assert.AreEqual(EqualityExpressions.CheckForEquality(()=>bla == bla2), EqualityExpressionClass.EqualsOp);
+		}
+		[Test]
+		public void NotEqualsOpDetected() {
+			Assert.AreEqual(EqualityExpressions.CheckForEquality(() => bla != bla2), EqualityExpressionClass.NotEqualsOp);
+		}
+		[Test]
+		public void EquatableDetected() {
+			Assert.AreEqual(EqualityExpressions.CheckForEquality(() => ((IEquatable<string>)bla).Equals(bla2string) ), EqualityExpressionClass.EquatableEquals);
+		}
+	}
+}
