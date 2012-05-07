d8a58c3ec8cd0afdf4241c6bf016449e0171f35d
Eamon Nerbonne <eamon@nerbonne.org>
2012-05-07 13:46:01 +0200

    PAssert: when expression evaluation causes exception, still pretty-print explanation why.

--- ExpressionToCodeLib/PAssert.cs
+++ ExpressionToCodeLib/PAssert.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Linq.Expressions;
 
 namespace ExpressionToCodeLib {
 	public static class PAssert {
 		public static void IsTrue(Expression<Func<bool>> assertion) {
-			That(assertion,"PAssert.IsTrue failed for:");
+			That(assertion, "PAssert.IsTrue failed for:");
 		}
 		public static void That(Expression<Func<bool>> assertion, string msg = null) {
-			if (!assertion.Compile()())
-				throw new PAssertFailedException((msg ?? "PAssert.That failed for:") + "\n\n" + ExpressionToCode.AnnotatedToCode(assertion.Body));
+			var compiled = assertion.Compile();
+			bool? ok;
+			try {
+				ok = compiled();
+			} catch (Exception e) {
+				throw new PAssertFailedException((msg ?? "PAssert.That failed for:") + "\n\n" +
+													 ExpressionToCode.AnnotatedToCode(assertion.Body), e);
+			}
+			if (ok == false)
+				throw new PAssertFailedException((msg ?? "PAssert.That failed for:") + "\n\n" +
+													 ExpressionToCode.AnnotatedToCode(assertion.Body));
 		}
 	}
 }
