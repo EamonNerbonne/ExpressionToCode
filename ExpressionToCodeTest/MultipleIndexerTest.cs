9c6b5b2ee1cb1776b0d2f96fa78acf1f3e8c50a8
Eamon Nerbonne <eamon@nerbonne.org>
2013-03-07 14:31:14 +0100

    Added unit test for issue 15
    The issue concerns code generation for types with multiple indexers.

---
+++ ExpressionToCodeTest/MultipleIndexerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExpressionToCodeLib;
+using NUnit.Framework;
+
+namespace ExpressionToCodeTest
+{
+	public class MultipleIndexerTest
+	{
+		class Bar
+		{
+			public object this[string s] { get { return null; } }
+			public object this[int i] { get { return null; } }
+		}
+
+		[Test]
+		public void CanPrettyPrintVariousIndexers() { 
+			Assert.AreEqual(
+				"() => new Bar()[3] == new Bar()[\"three\"]",
+				ExpressionToCode.ToCode(() => new Bar()[3] == new Bar()["three"])
+				);
+		}
+
+	}
+}
