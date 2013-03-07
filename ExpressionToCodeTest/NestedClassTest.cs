b0014b433e88d0a5efb2699665c9c49f0265e3b1
Eamon Nerbonne <eamon@nerbonne.org>
2013-03-07 14:35:15 +0100

    Added unit test for issue 16: nested type names aren't correctly regenerated.

---
+++ ExpressionToCodeTest/NestedClassTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using ExpressionToCodeLib;
+using NUnit.Framework;
+
+namespace ExpressionToCodeTest
+{
+	public class Parent
+	{
+		public class Child
+		{
+		}
+	}
+
+	class NestedClassTest
+	{
+		[Test]
+		public void NestedTypeNamesAreCorrectlyRegenerated()
+		{
+			string code = ExpressionToCode.ToCode(() => null as Parent.Child);
+			Assert.That(code, Is.EqualTo("() => null as Parent.Child"));
+		}
+	}
+}
