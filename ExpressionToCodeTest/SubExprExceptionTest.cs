2e0d6404e95b14c1488c74ec76a18a7cd0aa4555
Eamon Nerbonne <eamon@nerbonne.org>
2013-03-07 15:02:01 +0100

    Add test verifying that issue 20 is fixed.

---
+++ ExpressionToCodeTest/SubExprExceptionTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExpressionToCodeLib;
+using NUnit.Framework;
+
+namespace ExpressionToCodeTest
+{
+	public class FailingClass
+	{
+		public static int SomeFunction() { throw new Exception(); }
+	}
+
+	class SubExprExceptionTest
+	{
+		[Test]
+		public void ExceptionDoesntCauseFailure()
+		{
+			Assert.AreEqual(
+@"() => FailingClass.SomeFunction()
+                         |
+                         throws System.Exception
+", ExpressionToCode.AnnotatedToCode(() => FailingClass.SomeFunction()));
+		}
+
+	}
+}
