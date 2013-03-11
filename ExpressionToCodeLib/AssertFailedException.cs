405ad3f2629985809cc769f7c9d60d9b6ce0b4c1
Eamon Nerbonne <eamon@nerbonne.org>
2013-03-11 18:28:17 +0100

    Reintroduce (but obsolete) PAssertFailedException to avoid breaking changes.

--- ExpressionToCodeLib/AssertFailedException.cs
+++ ExpressionToCodeLib/AssertFailedException.cs
@@ -3,7 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace ExpressionToCodeLib {
-	sealed class AssertFailedException : Exception {
+	[Obsolete(
+		"This class is *not* the base class of all assertion violation exceptions - don't rely on it!  It will be removed in version 2."
+		)]
+	public class PAssertFailedException : Exception {
+		public PAssertFailedException(string message) : base(message) { }
+		public PAssertFailedException(string message, Exception inner) : base(message, inner) { }
+	}
+
+#pragma warning disable 618
+	sealed class AssertFailedException : PAssertFailedException
+#pragma warning restore 618
+	{
 		public AssertFailedException(string message) : base(message) { }
 		public AssertFailedException(string message, Exception inner) : base(message, inner) { }
 	}
