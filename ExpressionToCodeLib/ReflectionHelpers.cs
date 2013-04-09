8feed6c59d223a72e24488ac3fa1a0c903cb2f34
Eamon Nerbonne <eamon@nerbonne.org>
2013-04-09 14:47:50 +0200

    Fix obscure bug revolving around Compiler-generated closures that are generic.

--- ExpressionToCodeLib/ReflectionHelpers.cs
+++ ExpressionToCodeLib/ReflectionHelpers.cs
@@ -4,9 +4,12 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Collections.Generic;
 
-namespace ExpressionToCodeLib {
-	static class ReflectionHelpers {
-		public static PropertyInfo GetPropertyIfGetter(MethodInfo mi) {
+namespace ExpressionToCodeLib
+{
+	static class ReflectionHelpers
+	{
+		public static PropertyInfo GetPropertyIfGetter(MethodInfo mi)
+		{
 			bool supposedGetter = mi.Name.StartsWith("get_");
 			//bool supposedSetter = mi.Name.StartsWith("set_");
 
@@ -22,54 +25,59 @@ namespace ExpressionToCodeLib {
 					pars.Select(p => p.ParameterType).ToArray(), null);
 		}
 
-		public static bool IsMemberInfoStatic(MemberInfo mi) {
+		public static bool IsMemberInfoStatic(MemberInfo mi)
+		{
 			if (mi is FieldInfo)
-				return ((FieldInfo) mi).IsStatic;
+				return ((FieldInfo)mi).IsStatic;
 			else if (mi is MethodInfo)
-				return (((MethodInfo) mi).Attributes & MethodAttributes.Static) == MethodAttributes.Static;
-			else if (mi is PropertyInfo) {
-				PropertyInfo pi = (PropertyInfo) mi;
+				return (((MethodInfo)mi).Attributes & MethodAttributes.Static) == MethodAttributes.Static;
+			else if (mi is PropertyInfo)
+			{
+				PropertyInfo pi = (PropertyInfo)mi;
 				return pi.CanRead ? pi.GetGetMethod().IsStatic : pi.GetSetMethod().IsStatic;
-			} else if (mi.MemberType == MemberTypes.NestedType)
+			}
+			else if (mi.MemberType == MemberTypes.NestedType)
 				return true;
 			else if (mi is EventInfo)
-				return ((EventInfo) mi).GetAddMethod(true).IsStatic;
+				return ((EventInfo)mi).GetAddMethod(true).IsStatic;
 			else
 				throw new ArgumentOutOfRangeException("mi",
 					"Expression represents a member access for member" + mi.Name + " of membertype " + mi.MemberType
 						+ " that is unsupported");
 		}
 
-		public static bool HasBuiltinConversion(Type from, Type to) {
+		public static bool HasBuiltinConversion(Type from, Type to)
+		{
 			return
-				from == typeof (sbyte)
-					&& (to == typeof (short) || to == typeof (int) || to == typeof (long) || to == typeof (float)
-						|| to == typeof (double) || to == typeof (decimal))
-					|| from == typeof (byte)
-						&& (to == typeof (short) || to == typeof (ushort) || to == typeof (int) || to == typeof (uint)
-							|| to == typeof (long) || to == typeof (ulong) || to == typeof (float) || to == typeof (double)
-							|| to == typeof (decimal))
-					|| from == typeof (short)
-						&& (to == typeof (int) || to == typeof (long) || to == typeof (float) || to == typeof (double)
-							|| to == typeof (decimal))
-					|| from == typeof (ushort)
-						&& (to == typeof (int) || to == typeof (uint) || to == typeof (long) || to == typeof (ulong)
-							|| to == typeof (float) || to == typeof (double) || to == typeof (decimal))
-					|| from == typeof (int)
-						&& (to == typeof (long) || to == typeof (float) || to == typeof (double) || to == typeof (decimal))
-					|| from == typeof (uint)
-						&& (to == typeof (long) || to == typeof (ulong) || to == typeof (float) || to == typeof (double)
-							|| to == typeof (decimal))
-					|| from == typeof (long) && (to == typeof (float) || to == typeof (double) || to == typeof (decimal))
-					|| from == typeof (char)
-						&& (to == typeof (ushort) || to == typeof (int) || to == typeof (uint) || to == typeof (long)
-							|| to == typeof (ulong) || to == typeof (float) || to == typeof (double) || to == typeof (decimal))
-					|| from == typeof (float) && (to == typeof (double))
-					|| from == typeof (ulong) && (to == typeof (float) || to == typeof (double) || to == typeof (decimal))
+				from == typeof(sbyte)
+					&& (to == typeof(short) || to == typeof(int) || to == typeof(long) || to == typeof(float)
+						|| to == typeof(double) || to == typeof(decimal))
+					|| from == typeof(byte)
+						&& (to == typeof(short) || to == typeof(ushort) || to == typeof(int) || to == typeof(uint)
+							|| to == typeof(long) || to == typeof(ulong) || to == typeof(float) || to == typeof(double)
+							|| to == typeof(decimal))
+					|| from == typeof(short)
+						&& (to == typeof(int) || to == typeof(long) || to == typeof(float) || to == typeof(double)
+							|| to == typeof(decimal))
+					|| from == typeof(ushort)
+						&& (to == typeof(int) || to == typeof(uint) || to == typeof(long) || to == typeof(ulong)
+							|| to == typeof(float) || to == typeof(double) || to == typeof(decimal))
+					|| from == typeof(int)
+						&& (to == typeof(long) || to == typeof(float) || to == typeof(double) || to == typeof(decimal))
+					|| from == typeof(uint)
+						&& (to == typeof(long) || to == typeof(ulong) || to == typeof(float) || to == typeof(double)
+							|| to == typeof(decimal))
+					|| from == typeof(long) && (to == typeof(float) || to == typeof(double) || to == typeof(decimal))
+					|| from == typeof(char)
+						&& (to == typeof(ushort) || to == typeof(int) || to == typeof(uint) || to == typeof(long)
+							|| to == typeof(ulong) || to == typeof(float) || to == typeof(double) || to == typeof(decimal))
+					|| from == typeof(float) && (to == typeof(double))
+					|| from == typeof(ulong) && (to == typeof(float) || to == typeof(double) || to == typeof(decimal))
 				;
 		}
 
-		public static bool CanImplicitlyCast(Type from, Type to) {
+		public static bool CanImplicitlyCast(Type from, Type to)
+		{
 			return to.IsAssignableFrom(from) || HasBuiltinConversion(from, to);
 
 			//TODO: extend with op_Implicit support.
@@ -80,7 +88,8 @@ namespace ExpressionToCodeLib {
 			//if it does NOT compile, and IS implicit, then we can omit it since the compiler will add it.
 		}
 
-		public enum TypeClass {
+		public enum TypeClass
+		{
 			BuiltinType,
 			AnonymousType,
 			ClosureType,
@@ -88,14 +97,16 @@ namespace ExpressionToCodeLib {
 			NormalType,
 		}
 
-		public static TypeClass GuessTypeClass(this Type type) {
-			bool compilerGenerated = type.GetCustomAttributes(typeof (CompilerGeneratedAttribute), false).Any();
+		public static TypeClass GuessTypeClass(this Type type)
+		{
+			bool compilerGenerated = type.GetCustomAttributes(typeof(CompilerGeneratedAttribute), false).Any();
 			string name = type.Name;
 			bool name_StartWithLessThan = name.StartsWith("<");
-			bool isBuiltin = type.IsPrimitive || type.IsEnum || type == typeof (decimal) || type == typeof (string)
-				|| typeof (Type).IsAssignableFrom(type);
+			bool isBuiltin = type.IsPrimitive || type.IsEnum || type == typeof(decimal) || type == typeof(string)
+				|| typeof(Type).IsAssignableFrom(type);
 
-			if (name_StartWithLessThan && compilerGenerated) {
+			if (name_StartWithLessThan && compilerGenerated)
+			{
 				bool named_AnonymousType = name.Contains("AnonymousType");
 				bool named_DisplayClass = name.Contains("DisplayClass");
 				bool isGeneric = type.IsGenericType;
@@ -103,12 +114,13 @@ namespace ExpressionToCodeLib {
 
 				if (!isBuiltin && isGeneric && !isNested && named_AnonymousType)
 					return TypeClass.AnonymousType;
-				else if (!isBuiltin && !isGeneric && isNested && named_DisplayClass)
+				else if (!isBuiltin && isNested && named_DisplayClass)
 					return TypeClass.ClosureType;
-						//note that since genericness+nestedness don't overlap, these typeclasses aren't confusable.
+				//note that since genericness+nestedness don't overlap, these typeclasses aren't confusable.
 				else
-					throw new ArgumentException("Can't deal with unknown-style compiler generated class " + type.FullName);
-			} else if (!compilerGenerated && !name_StartWithLessThan)
+					throw new ArgumentException("Can't deal with unknown-style compiler generated class " + type.FullName + " " + named_AnonymousType + ", " + named_DisplayClass + ", " + isGeneric + ", " + isNested);
+			}
+			else if (!compilerGenerated && !name_StartWithLessThan)
 				return isBuiltin ? TypeClass.BuiltinType : type.IsValueType ? TypeClass.StructType : TypeClass.NormalType;
 			else
 				throw new ArgumentException("Unusual type, heuristics uncertain:" + name);
