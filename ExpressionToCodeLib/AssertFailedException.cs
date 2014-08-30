5a41803c9ede36c0de46a0eb2f05fabe1647ae70
Eamon Nerbonne <eamon@nerbonne.org>
2014-08-30 12:19:58 +0200

    Make AssertFailedException Serializable.

--- ExpressionToCodeLib/AssertFailedException.cs
+++ ExpressionToCodeLib/AssertFailedException.cs
@@ -1,21 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 
 namespace ExpressionToCodeLib {
 	[Obsolete(
 		"This class is *not* the base class of all assertion violation exceptions - don't rely on it!  It will be removed in version 2."
-		)]
+		), Serializable]
 	public class PAssertFailedException : Exception {
 		public PAssertFailedException(string message) : base(message) { }
 		public PAssertFailedException(string message, Exception inner) : base(message, inner) { }
+		public PAssertFailedException(SerializationInfo info, StreamingContext context) : base(info, context) { }
 	}
 
+
 #pragma warning disable 618
+	[Serializable]
 	sealed class AssertFailedException : PAssertFailedException
 #pragma warning restore 618
-	{
+ {
 		public AssertFailedException(string message) : base(message) { }
 		public AssertFailedException(string message, Exception inner) : base(message, inner) { }
+		public AssertFailedException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+	}
+
+	namespace Internal {
+
+		/// <summary>
+		/// This class is not part of the public API: it's undocumented and minor version bumps may break compatiblity.
+		/// </summary>
+		public static class UnitTestingInternalsAccess {
+			public static Exception CreateException(string msg) {
+				return new AssertFailedException(msg);
+			}
+		}
 	}
 }
---
+++ ExpressionToCodeTest/ExceptionsSerialization.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using ExpressionToCodeLib;
+using ExpressionToCodeLib.Internal;
+using NUnit.Framework;
+
+namespace ExpressionToCodeTest {
+	public class ExceptionsSerialization {
+		[MethodImpl(MethodImplOptions.NoInlining)]
+		static void IntentionallyFailingMethod() {
+			PAssert.That(() => false);
+		}
+		[MethodImpl(MethodImplOptions.NoInlining)]
+		static void IntentionallyFailingMethod2() {
+			throw UnitTestingInternalsAccess.CreateException("Hello World!");
+		}
+
+		[Test]
+		public void NUnitExceptionIsSerializable() {
+			AssertMethodFailsWithSerializableException(IntentionallyFailingMethod);
+		}
+		[Test]
+		public void PAssertExceptionIsSerializable() {
+			AssertMethodFailsWithSerializableException(IntentionallyFailingMethod2);
+		}
+
+		static void AssertMethodFailsWithSerializableException(TestDelegate intentionallyFailingMethod) {
+			var original = Assert.Catch<Exception>(intentionallyFailingMethod);
+
+			var formatter = new BinaryFormatter();
+			var ms = new MemoryStream();
+			formatter.Serialize(ms, original);
+			object deserialized = formatter.Deserialize(new MemoryStream(ms.ToArray()));
+
+			Assert.AreEqual(original.ToString(), deserialized.ToString());
+		}
+	}
+}
