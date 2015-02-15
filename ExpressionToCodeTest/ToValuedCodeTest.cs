7955bbcab88ce090b25d095d83f5606934d29ca9
Eamon Nerbonne <eamon@nerbonne.org>
2015-02-15 10:02:08 +0100

    Minor resharper cleanups of ToValuedCodeTest.
    Also prefer Assert.Catch over ExpectedException (which is a code smell due to its lack of precision and clarity)

--- ExpressionToCodeTest/ToValuedCodeTest.cs
+++ ExpressionToCodeTest/ToValuedCodeTest.cs
@@ -1,43 +1,29 @@
 ﻿using ExpressionToCodeTest.Unstable_v2_Api;
-// ReSharper disable RedundantEnumerableCastCall
-// ReSharper disable RedundantNameQualifier
-// ReSharper disable ConvertToConstant.Local
-// ReSharper disable RedundantLogicalConditionalExpressionOperand
-// ReSharper disable RedundantCast
-// ReSharper disable ConstantNullCoalescingCondition
-// ReSharper disable EqualExpressionComparison
-// ReSharper disable RedundantToStringCall
-
-#pragma warning disable 1720
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Xml;
 using NUnit.Framework;
-using ExpressionToCodeLib;
 
 namespace ExpressionToCodeTest
 {
     [TestFixture]
     public class ToValuedCodeTest
     {
-   
         [Test]
-        [ExpectedException(typeof(InvalidOperationException))]
         public void ToValuedCode_ofNull_fails()
         {
             ExpressionToCodeTest obj = null;
-            var actual = ExpressionWithValue.ToValuedCode(() => obj.TheProperty);
+            Assert.Catch<InvalidOperationException>(
+                () =>
+                    ExpressionWithValue.ToValuedCode(() => obj.TheProperty));
         }
-
 
         [Test]
         public void ArrayLength()
         {
-            var arr = new int[] { 1 };
+            var arr = new[] { 1 };
             var actual = ExpressionWithValue.ToValuedCode(() => arr.Length);
-            Assert.AreEqual("arr.Length = 1",actual);
+            Assert.AreEqual("arr.Length = 1", actual);
         }
 
         [Test]
@@ -46,8 +32,6 @@ namespace ExpressionToCodeTest
             var actual = ExpressionWithValue.ToValuedCode(() => TheProperty);
             Assert.AreEqual("TheProperty = TheValue", actual);
         }
-
-
 
         [Test]
         public void TheStringVariable()
@@ -60,16 +44,14 @@ namespace ExpressionToCodeTest
         [Test]
         public void TheMethod()
         {
-            Expression<Func<string>> theMethod = () => TheMethod(1, "2");  //can be invoked later
-            var actual = ExpressionWithValue.ToValuedCode(theMethod);
+            var actual = ExpressionWithValue.ToValuedCode(() => TheMethod(1, "2"));
             Assert.AreEqual("TheMethod(1, \"2\") = TheMethod 1 2", actual);
         }
 
         [Test]
         public void TheGenericMethod()
         {
-            Expression<Func<string>> theGenericMethod = () => TheGenericMethod<int>(2);
-            var actual = ExpressionWithValue.ToValuedCode(theGenericMethod);
+            var actual = ExpressionWithValue.ToValuedCode(() => TheGenericMethod<int>(2));
             Assert.AreEqual("TheGenericMethod<Int32>(2) = Return value is 4", actual);
         }
 
@@ -79,7 +61,6 @@ namespace ExpressionToCodeTest
             var actual = ExpressionWithValue.ToValuedCode(() => this[1]);
             Assert.AreEqual("ToValuedCodeTest[1] = TheIndexedValue", actual);
         }
-
 
         [Test]
         public void ThisMethodCall()
@@ -97,40 +78,14 @@ namespace ExpressionToCodeTest
             Assert.AreEqual("ToValuedCodeTest.StaticReturnZero() = 0", code);
         }
 
-        public string TheProperty
-        {
-            get
-            {
-                return "TheValue";
-            }
-        }
-
-        public string this[int index]
-        {
-            get
-            {
-                return "TheIndexedValue";
-            }
-        }
-
-        public string TheMethod(int parameter1, string parameter2)
-        {
-            return "TheMethod " + parameter1 + " " + parameter2;
-        }
-
-        public string TheGenericMethod<T>(int two)
-        {
-            return "Return value is " + two * two;
-        }
-
-        public int ReturnZero()
-        {
-            return 0;
-        }
-
-        public static int StaticReturnZero()
-        {
-            return 0;
-        }
+        static string TheProperty { get { return "TheValue"; } }
+        // ReSharper disable once UnusedParameter.Local
+        string this[int index] { get { return "TheIndexedValue"; } }
+        static int StaticReturnZero() { return 0; }
+        // ReSharper disable MemberCanBeMadeStatic.Local
+        string TheMethod(int parameter1, string parameter2) { return "TheMethod " + parameter1 + " " + parameter2; }
+        // ReSharper disable once UnusedTypeParameter
+        string TheGenericMethod<T>(int two) { return "Return value is " + two * two; }
+        int ReturnZero() { return 0; }
     }
 }
