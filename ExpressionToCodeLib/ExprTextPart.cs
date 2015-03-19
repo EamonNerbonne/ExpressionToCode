f47a95ab2cbf3b1c5f57b64a0257be26af666119
Eamon Nerbonne <eamon@nerbonne.org>
2015-03-19 09:02:13 +0100

    Reduce PR size.

--- ExpressionToCodeLib/ExprTextPart.cs
+++ ExpressionToCodeLib/ExprTextPart.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Linq.Expressions;
 
-namespace ExpressionToCodeLib {
-    class ExprTextPart {
+namespace ExpressionToCodeLib
+{
+    class ExprTextPart
+    {
         public readonly string Text;
         public readonly Expression OptionalValue;
 
-        ExprTextPart(string text, Expression val) {
-            if (text == null) {
+        ExprTextPart(string text, Expression val)
+        {
+            if(text == null) {
                 throw new ArgumentNullException("text");
             }
             Text = text;
@@ -16,8 +19,9 @@ namespace ExpressionToCodeLib {
 
         public static ExprTextPart TextOnly(string text) { return new ExprTextPart(text, null); }
 
-        public static ExprTextPart TextAndExpr(string text, Expression expr) {
-            if (expr == null) {
+        public static ExprTextPart TextAndExpr(string text, Expression expr)
+        {
+            if(expr == null) {
                 throw new ArgumentNullException("expr");
             }
             return new ExprTextPart(text, expr);
