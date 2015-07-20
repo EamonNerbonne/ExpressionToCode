8786376a9ef7ef9eb972ae8964e7e69cb9fa962f
dzmitry lahoda <dzmitry.lahoda@gmail.com>
2015-07-20 18:26:34 +0300

    - added to name  method

---
+++ ExpressionToCodeLib/Unstable_v2_Api/ExpressionWithName.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ExpressionToCodeLib.Unstable_v2_Api
+{
+	public static class ExpressionWithName
+	{
+		/// <summary>
+		/// Gets property, variable or method name from lambda expression.
+		/// </summary>
+		/// <param name="expression"></param>
+		/// <returns></returns>
+		/// <example>
+		/// var example = "some text";
+		/// var name = toName( () => example);  // "example"
+		/// </example>
+		public static string ToName<TResult>(this Expression<Func<TResult>> expression)
+		{
+			string value = ToName(expression);
+			return value;
+		}
+
+		public static string ToName<T1, T2, TResult>(this Expression<Func<T1, T2, TResult>> expression)
+		{
+			string value = ToName(expression);
+			return value;
+		}
+
+		public static string ToName<T1, TResult>(this Expression<Func<T1, TResult>> expression)
+		{
+			string value = ToName(expression);
+			return value;
+		}
+
+		public static string ToName<T>(this Expression<Action<T>> expression)
+		{
+			string value = ToName(expression);
+			return value;
+		}
+
+
+		public static string ToName(this Expression<Action> expression)
+		{
+			string value = ToName(expression);
+			return value;
+		}
+
+
+
+		public static string ToName<T>(Expression<T> expression)
+		{
+			string value = null;
+			var unaryExpression = expression.Body as UnaryExpression;
+			if (unaryExpression != null)
+			{
+				value = unaryExpression.Operand.ToString().Split('.').Last();
+			}
+			var memberExpression = expression.Body as MemberExpression;
+			if (memberExpression != null)
+			{
+				value = memberExpression.Member.Name;
+			}
+			var methodCallExpression = expression.Body as MethodCallExpression;
+			if (methodCallExpression != null)
+			{
+				var methodName = methodCallExpression.Method.Name;
+				value = methodName;
+			}
+			if (value == null)
+				throw new ArgumentException("expression", "Unknown expression");
+			return value;
+		}
+
+		//NOTE: should use recursive visitor as in other method when new failed test case added
+		public static string ToFullName<T>(this Expression<T> expression)
+		{
+			string name = null;
+			var unaryExpression = expression.Body as UnaryExpression;
+			if(unaryExpression != null) {
+				name = unaryExpression.Operand.ToString().Split('.').Last();
+				if(unaryExpression.NodeType == ExpressionType.ArrayLength) {
+					name += ".Length";
+				}
+			}
+			var memberExpression = expression.Body as MemberExpression;
+			if(memberExpression != null) {
+				name = memberExpression.Member.Name;
+			}
+			var methodCallExpression = expression.Body as MethodCallExpression;
+			if(methodCallExpression != null) {
+				// tries transform method and return value in human readable C#-style representation
+
+				// add declaring type if it is not a module
+				var arguments = string.Join(
+					", ",
+					methodCallExpression.Arguments.Select(x => x.ToString()).ToArray() // converting to string to work for .NET 3.5 if backported
+				);
+				var method = methodCallExpression.Method;
+				var methodName = method.Name;
+				if(method.IsGenericMethod) {
+					methodName += "<" + String.Join(
+						", ",
+						method.GetGenericArguments().Select(x => x.Name).ToArray()) // converting to string to work for .NET 3.5 if backported
+						+ ">";
+				}
+				if(methodName == "get_Item" && methodCallExpression.Arguments.Count > 0) {
+					//indexed property
+					string typeName = methodCallExpression.Object != null ? methodCallExpression.Object.Type.Name : "";
+					name = typeName + "[" + arguments + "]";
+				} else {
+					var typePrefix = "";
+					if(method.IsStatic) {
+						typePrefix = method.DeclaringType.Name + ".";
+					}
+					name = typePrefix + methodName + "(" + arguments + ")";
+				}
+			}
+			if(name == null) {
+				throw new ArgumentException("Failed to translate expression to its valued representation", "expression");
+			}
+			return name;
+		}
+	}
+}
---
+++ ExpressionToCodeTest/ExpressionWithNameTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using ExpressionToCodeLib.Unstable_v2_Api;
+using Xunit;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ExpressionToCodeTest
+{
+	public class ExpressionWithNameTest
+	{
+		[Fact]
+		public void TheVariable_ToName()
+		{
+			var theVariable = "theValue";
+			var actual = ExpressionWithName.ToName(() => theVariable);
+			Assert.Equal("theVariable",actual);
+		}
+
+
+
+		[Fact]
+		public void TheMethod_ToName()
+		{
+			Expression<Func<int, string, string>> theMethod = (x, y) => ExpressionWithNameTest.TheMethod(x, y);
+			var actual = ExpressionWithName.ToName(theMethod);
+			Assert.Equal("TheMethod",actual);
+		}
+
+		[Fact]
+		public void TheMethod_ToFullName()
+		{
+			Expression<Func<int, string, string>> theMethod = (x, y) => ExpressionWithNameTest.TheMethod(x, y);
+			var actual = ExpressionWithName.ToFullName(theMethod);
+			Assert.Equal("ExpressionWithNameTest.TheMethod(x, y)",actual);
+		}
+
+
+		[Fact]
+		public void TheGenericMethod_ToName()
+		{
+			Expression<Func<string>> theGenericMethod = () => ExpressionWithNameTest.TheGenericMethod<int>(2);
+			var actual = ExpressionWithName.ToName(theGenericMethod);
+			Assert.Equal("TheGenericMethod",actual);
+		}
+
+
+
+		[Fact]
+		public void TheSimpleMethod_ToName()
+		{
+			Expression<Action> theSimpleMethod = () => TheSimpleMethod(); 
+			var actual = theSimpleMethod.ToName();
+			Assert.Equal("TheSimpleMethod",actual);
+		}
+
+		public void TheSimpleMethod()
+		{
+		}
+		static string TheProperty { get { return "TheValue"; } }
+		// ReSharper disable once UnusedParameter.Local
+		string this[int index] { get { return "TheIndexedValue"; } }
+		static int StaticReturnZero() { return 0; }
+		// ReSharper disable MemberCanBeMadeStatic.Local
+		static string TheMethod(int parameter1, string parameter2) { return "TheMethod " + parameter1 + " " + parameter2; }
+		// ReSharper disable once UnusedTypeParameter
+		static string TheGenericMethod<T>(int two) { return "Return value is " + two * two; }
+		int ReturnZero() { return 0; }
+	}
+}
+
