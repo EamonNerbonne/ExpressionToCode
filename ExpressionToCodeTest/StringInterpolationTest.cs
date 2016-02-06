edd5afb084685a4f89d080acc87f4f355636a855
Eamon Nerbonne <eamon@nerbonne.org>
2016-02-06 21:55:11 +0100

    Added string interpolation test

---
+++ ExpressionToCodeTest/StringInterpolationTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExpressionToCodeLib;
+using Xunit;
+
+namespace ExpressionToCodeTest
+{
+    public class StringInterpolationTest
+    {
+        [Fact]
+        public void BasicStringInterpolation()
+        {
+            Assert.Equal(@"() => $""abc""",
+                ExpressionToCode.ToCode(() => $"abc"));
+        }
+    }
+}
