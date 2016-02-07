d6f2639619dc69426b70cbf86d2fbb7b593199d2
Eamon Nerbonne <eamon@nerbonne.org>
2016-02-08 00:45:47 +0100

    Drop PAssertException from public api

--- ExpressionToCodeLib/AssertFailedException.cs
+++ ExpressionToCodeLib/AssertFailedException.cs
@@ -5,24 +5,8 @@ using System.Runtime.Serialization;
 
 namespace ExpressionToCodeLib
 {
-    [Obsolete(
-        "This class is *not* the base class of all assertion violation exceptions - don't rely on it!  It will be removed in version 2."
-        ), Serializable]
-    public class PAssertFailedException : Exception
-    {
-        public PAssertFailedException(string message)
-            : base(message) { }
-
-        public PAssertFailedException(string message, Exception inner)
-            : base(message, inner) { }
-
-        public PAssertFailedException(SerializationInfo info, StreamingContext context)
-            : base(info, context) { }
-    }
-#pragma warning disable 618
     [Serializable]
-    sealed class AssertFailedException : PAssertFailedException
-#pragma warning restore 618
+    sealed class AssertFailedException : Exception
     {
         public AssertFailedException(string message)
             : base(message) { }
