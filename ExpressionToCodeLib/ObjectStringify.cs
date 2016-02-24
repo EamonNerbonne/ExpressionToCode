83555b07437f789f96dfa58237d212ba87b55d27
Eamon Nerbonne <eamon@nerbonne.org>
2016-02-24 15:21:13 +0100

    Place ObjectStringify into public api

---
+++ ExpressionToCodeLib/ObjectStringify.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using ExpressionToCodeLib.Internal;
+
+namespace ExpressionToCodeLib
+{
+    public static class ObjectStringify
+    {
+        public static readonly IObjectStringifier Default = new ObjectStringifyImpl();
+        public static readonly IObjectStringifier WithFullTypeNames = new ObjectStringifyImpl(true);
+    }
+}
