a4f4985628ff0ad42669305a1eea8d5c5064b9c7
Eamon Nerbonne <eamon@nerbonne.org>
2017-01-24 14:01:13 +0100

    Remove info that's now elsewhere.

--- ExpressionToCodeLib/Properties/AssemblyInfo.cs
+++ ExpressionToCodeLib/Properties/AssemblyInfo.cs
@@ -9,24 +9,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 [assembly: AssemblyTitle("ExpressionToCodeLib")]
-[assembly:
-    AssemblyDescription(
-        @"Create readable C# assertions (or other code) from an expression tree; can annotate subexpressions with their runtime value.  Integrates with xUnit.NET, NUnit and MSTest."
-        )]
-[assembly: AssemblyProduct("ExpressionToCodeLib")]
-[assembly: AssemblyCompany("Eamon Nerbonne")]
-[assembly: AssemblyCopyright("Copyright © Eamon Nerbonne")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("008ee713-7baa-491a-96f3-0a8ce3a473b1")]
-[assembly: AssemblyVersion("1.9.9")]
 
 
 */
