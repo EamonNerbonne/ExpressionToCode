33e22b2eabb930b2cba0621dcf25bf3e08f27877
Eamon Nerbonne <eamon@nerbonne.org>
2017-01-26 12:31:13 +0100

    Remove some junk

--- ExpressionToCodeLib/Properties/AssemblyInfo.cs
+++ ExpressionToCodeLib/Properties/AssemblyInfo.cs
@@ -1,18 +1,3 @@
-﻿/*  DOESN'T WORK YET!
- 
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyTitle("ExpressionToCodeLib")]
-
-
-*/
-
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("ExpressionToCodeTest")]
