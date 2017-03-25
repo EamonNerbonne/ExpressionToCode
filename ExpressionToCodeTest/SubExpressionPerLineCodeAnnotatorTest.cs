bb2bd3ad045a207e1708b216cceb37b25f4d215f
Eamon Nerbonne <eamon@nerbonne.org>
2017-03-25 10:44:13 +0100

    Some initial approvals

---
+++ ExpressionToCodeTest/SubExpressionPerLineCodeAnnotatorTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpressionToCodeLib;
+using Xunit;
+
+namespace ExpressionToCodeTest
+{
+    public class SubExpressionPerLineCodeAnnotatorTest
+    {
+        static readonly ExpressionToCodeConfiguration config = ExpressionToCodeConfiguration.DefaultAssertionConfiguration.WithAnnotator(CodeAnnotators.SubExpressionPerLineCodeAnnotator);
+        static readonly IAnnotatedToCode annotator = config.GetAnnotatedToCode();
+
+        [Fact]
+        public void AplusBapproved()
+        {
+            var a = 2;
+            var b = 5;
+            ApprovalTest.Verify(annotator.AnnotatedToCode(() => a + b));
+        }
+
+        [Fact]
+        public void NestedArrayAccess()
+        {
+            var a = 2;
+            var b = 5;
+            var nums = Enumerable.Range(10, 10).ToArray();
+            ApprovalTest.Verify(annotator.AnnotatedToCode(() => nums[a + b] < 7));
+        }
+
+        [Fact]
+        public void NestedArrayAccessWithOuterAnd()
+        {
+            var a = 2;
+            var b = 5;
+            var nums = Enumerable.Range(10, 10).ToArray();
+            ApprovalTest.Verify(annotator.AnnotatedToCode(() => a < b && nums[a + b] < 7 && b < 10));
+        }
+    }
+}
