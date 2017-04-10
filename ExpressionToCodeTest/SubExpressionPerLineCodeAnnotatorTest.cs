3c7bfe4f4cb9b132aa91fb6a416061ce109075e4
Eamon Nerbonne <eamon@nerbonne.org>
2017-04-10 10:09:40 +0200

    Bump version and reformat test

--- ExpressionToCodeTest/SubExpressionPerLineCodeAnnotatorTest.cs
+++ ExpressionToCodeTest/SubExpressionPerLineCodeAnnotatorTest.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using ExpressionToCodeLib;
 using System.Collections.Generic;
 using System.Linq;
-using ExpressionToCodeLib;
 using Xunit;
 
 namespace ExpressionToCodeTest
@@ -22,48 +22,12 @@ namespace ExpressionToCodeTest
         }
 
         [Fact]
-        public void NestedArrayAccess()
-        {
-            var a = 2;
-            var b = 5;
-            var nums = Enumerable.Range(10, 10).ToArray();
-            ApprovalTest.Verify(annotator.AnnotatedToCode(() => nums[a + b] < 7));
-        }
-
-        [Fact]
-        public void NestedArrayAccessWithOuterAnd()
-        {
-            var a = 2;
-            var b = 5;
-            var nums = Enumerable.Range(10, 10).ToArray();
-            ApprovalTest.Verify(annotator.AnnotatedToCode(() => a < b && nums[a + b] < 7 && b < 10));
-        }
-
-        [Fact]
         public void Binary_expressions_with_nesting()
         {
             var a = 2;
             var b = 5;
             var c = 3.45;
             ApprovalTest.Verify(annotator.AnnotatedToCode(() => a < b && (c > -a || c > b) && b < 10));
-        }
-
-        [Fact]
-        public void MethodCallsAndArrayLiterals()
-        {
-            var a = 2;
-            var b = 5;
-            ApprovalTest.Verify(annotator.AnnotatedToCode(() => Math.Max(a, b) > new[] { 3, 8, 13, 4 }.Average()));
-        }
-
-        [Fact]
-        public void MessyStructureElidesNeatly()
-        {
-            var hmm = "1234567890";
-            ApprovalTest.Verify(
-                annotator.AnnotatedToCode(
-                    () => hmm[1] == hmm[2] || hmm[4] == hmm[int.Parse(hmm[8].ToString())] || false
-                ));
         }
 
         [Fact]
@@ -82,6 +46,55 @@ namespace ExpressionToCodeTest
                 annotator.AnnotatedToCode(
                     () => string.Join("##", Enumerable.Range(0, 100)) + "suffix"
                 ));
+        }
+
+        [Fact]
+        public void DealsOkWithObjectsContainingLongStrings()
+        {
+            ApprovalTest.Verify(
+                annotator.AnnotatedToCode(
+                    () => new {
+                        A_long_string = string.Join("##", Enumerable.Range(0, 100)) + "suffix",
+                        A_short_string = "short",
+                        A_long_enumerable = Enumerable.Range(0, 1000)
+                    }
+                ));
+        }
+
+        [Fact]
+        public void MessyStructureElidesNeatly()
+        {
+            var hmm = "1234567890";
+            ApprovalTest.Verify(
+                annotator.AnnotatedToCode(
+                    () => hmm[1] == hmm[2] || hmm[4] == hmm[int.Parse(hmm[8].ToString())] || false
+                ));
+        }
+
+        [Fact]
+        public void MethodCallsAndArrayLiterals()
+        {
+            var a = 2;
+            var b = 5;
+            ApprovalTest.Verify(annotator.AnnotatedToCode(() => Math.Max(a, b) > new[] { 3, 8, 13, 4 }.Average()));
+        }
+
+        [Fact]
+        public void NestedArrayAccess()
+        {
+            var a = 2;
+            var b = 5;
+            var nums = Enumerable.Range(10, 10).ToArray();
+            ApprovalTest.Verify(annotator.AnnotatedToCode(() => nums[a + b] < 7));
+        }
+
+        [Fact]
+        public void NestedArrayAccessWithOuterAnd()
+        {
+            var a = 2;
+            var b = 5;
+            var nums = Enumerable.Range(10, 10).ToArray();
+            ApprovalTest.Verify(annotator.AnnotatedToCode(() => a < b && nums[a + b] < 7 && b < 10));
         }
     }
 }
