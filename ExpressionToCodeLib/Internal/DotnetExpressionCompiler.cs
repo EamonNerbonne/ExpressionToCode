c5d110169f0da17cbb4a2c623581377a1ce99031
Eamon Nerbonne <eamon@nerbonne.org>
2017-05-05 13:26:23 +0200

    When running on .net core, prefer expression tree interpretation.
    This appears to be several times faster than compilation, probably because it both avoid the JIT overhead, and because the expression-tree-to-il compiler is fairly slow too.

--- ExpressionToCodeLib/Internal/DotnetExpressionCompiler.cs
+++ ExpressionToCodeLib/Internal/DotnetExpressionCompiler.cs
@@ -6,6 +6,10 @@ namespace ExpressionToCodeLib.Internal
     class DotnetExpressionCompiler : IExpressionCompiler
     {
         public Func<T> Compile<T>(Expression<Func<T>> expression)
+#if dotnet_core
+            => expression.Compile(true);
+#else
             => expression.Compile();
+#endif
     }
 }
