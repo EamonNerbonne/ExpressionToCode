67837c72c1572dafe9805ac9d490c2dcb3331c4d
Eamon Nerbonne <eamon@nerbonne.org>
2018-06-01 13:02:20 +0200

    It's not even possible to express that expression tree.

--- ExpressionToCodeTest/ValueTupleTests.cs
+++ ExpressionToCodeTest/ValueTupleTests.cs
@@ -17,14 +17,16 @@ namespace ExpressionToCodeTest {
             Expression<Func<int>> ok1 = () => tuple.Item1;
             Expression<Func<int>> ok2 = () => tuple.GetHashCode();
             Expression<Func<Tuple<int, int>>> ok3 = () => tuple.ToTuple();
-            ok1.Compile();
-            ok2.Compile();
-            ok3.Compile();
+            Expression<Func<bool>> ok4 = () => Equals(tuple, tuple2);
+            Expression<Func<int>> ok5 = () => Comparer<(int, int)>.Default.Compare(tuple, tuple2);
+            ok1.Compile()();
+            ok2.Compile()();
+            ok3.Compile()();
+            ok4.Compile()();
+            ok5.Compile()();
 
-            Expression<Func<bool>> err1 = () => tuple.Equals(tuple2);
-            Expression<Func<int>> err2 = () => tuple.CompareTo(tuple2);
-            err1.Compile();//crash
-            err2.Compile();//crash
+            Expression<Func<bool>> err1 = () => tuple.Equals(tuple2);//crash
+            Expression<Func<int>> err2 = () => tuple.CompareTo(tuple2);//crash
         }
 
         [Fact]
