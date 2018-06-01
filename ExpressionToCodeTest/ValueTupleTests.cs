182f503c08507355faf1612081dde40c4dca5949
Eamon Nerbonne <eamon@nerbonne.org>
2018-06-01 12:31:32 +0200

    Add tests for value tuples

---
+++ ExpressionToCodeTest/ValueTupleTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using ExpressionToCodeLib;
+using Xunit;
+
+namespace ExpressionToCodeTest {
+    public class ValueTupleTests {
+        [Fact]
+        public void ExpressionCompileValueTupleEqualsWorks() {
+            var tuple = (1, 3);
+            var tuple2 = (1, "123".Length);
+            Expression<Func<bool>> expr = () => tuple.Equals(tuple2);
+            Assert.True(expr.Compile()());
+        }
+
+        [Fact]
+        public void FastExpressionCompileValueTupleEqualsWorks() {
+            var tuple = (1, 3);
+            (int, int Length) tuple2 = (1, "123".Length);
+            ValueTuple<int,int> x;
+            var expr = FastExpressionCompiler.ExpressionCompiler.Compile(() => tuple.Equals(tuple2));
+            Assert.True(expr());
+        }
+
+        [Fact]
+        public void AssertingOnValueTupleEqualsWorks() {
+            var tuple = (1, 3);
+            var tuple2 = (1, "123".Length);
+            Expression<Func<bool>> expr = () => tuple.Equals(tuple2);
+            Assert.True(expr.Compile()());
+            PAssert.That(() => tuple.Equals(tuple2));
+        }
+    }
+}
