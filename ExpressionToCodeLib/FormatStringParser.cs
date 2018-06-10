d9c453a20c2e2a147d6c84693a47406886021709
Eamon Nerbonne <eamon@nerbonne.org>
2018-06-10 21:11:12 +0200

    Add implementation of FormatStringParser

---
+++ ExpressionToCodeLib/FormatStringParser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExpressionToCodeLib
+{
+    public static class FormatStringParser
+    {
+        public struct Segment
+        {
+            public string StringPart;
+            public object FollowedByValue;
+            public string WithFormatString;
+        }
+
+#if !NET452
+        public static (Segment[] segments, string Tail) ParseFormatString(FormattableString formattableString)
+            => new FormattableStringParser(formattableString.Format, formattableString.GetArguments()).Finish();
+#endif
+
+        public static (Segment[] segments, string Tail) ParseFormatString(string formatString, object[] formatArguments)
+            => new FormattableStringParser(formatString, formatArguments).Finish();
+
+        class FormattableStringParser : IFormatProvider, ICustomFormatter
+        {
+            //Format strings have exceptions for stuff like double curly braces
+            //There are also corner-cases that non-compiler generated format strings might hit
+            //All in all: rather than reimplement a custom parser, this 
+            readonly StringBuilder sb = new StringBuilder();
+            readonly List<Segment> Segments = new List<Segment>();
+
+            public FormattableStringParser(string formatString, object[] formatArguments)
+                => sb.AppendFormat(this, formatString, formatArguments);
+
+            object IFormatProvider.GetFormat(Type formatType)
+                => this;
+
+            public (Segment[] segments, string Tail) Finish()
+                => (Segments.ToArray(), sb.ToString());
+
+            string ICustomFormatter.Format(string format, object arg, IFormatProvider formatProvider)
+            {
+                Segments.Add(
+                    new Segment {
+                        StringPart = sb.ToString(),
+                        FollowedByValue = arg,
+                        WithFormatString = format,
+                    });
+                sb.Length = 0;
+                return "";
+            }
+        }
+    }
+}
