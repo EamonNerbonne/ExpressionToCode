928bd0df08b06139d57162675ccf2896966868d1
Eamon Nerbonne <eamon@nerbonne.org>
2020-09-12 10:44:38 +0200

    Add an expression interpretation bug test

---
+++ ExpressionToCodeTest/ExpressionInterpretationBug.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace ExpressionToCodeTest
+{
+    public class ExpressionInterpretationBug
+    {
+        struct AStruct
+        {
+            public int AValue;
+        }
+
+        class SomethingMutable
+        {
+            public AStruct AStructField;
+        }
+
+        static Expression<Func<T>> Expr<T>(Expression<Func<T>> e)
+            => e;
+
+        [Fact]
+        public void DotNetCoreIsNotBuggy()
+        {
+            var expr_compiled = Expr(() => new SomethingMutable { AStructField = { AValue = 2 } }).Compile(false)();
+            Assert.Equal(2, expr_compiled.AStructField.AValue);
+
+            var expr_interpreted = Expr(() => new SomethingMutable { AStructField = { AValue = 2 } }).Compile(true)();
+            Assert.Equal(2, expr_interpreted.AStructField.AValue);
+        }
+    }
+}
