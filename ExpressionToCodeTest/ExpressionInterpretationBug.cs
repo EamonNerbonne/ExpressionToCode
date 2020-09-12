4091ea0de9e382e7c3a4cb35ca55fd0848f1bbbd
Eamon Nerbonne <eamon@nerbonne.org>
2020-09-12 11:13:25 +0200

    Assert on the *buggy* behavior, so I notice when it gets fixed

--- ExpressionToCodeTest/ExpressionInterpretationBug.cs
+++ ExpressionToCodeTest/ExpressionInterpretationBug.cs
@@ -1,10 +1,14 @@
 ﻿#if expression_interpretation
 using System;
 using System.Linq.Expressions;
+using FastExpressionCompiler;
 using Xunit;
 
 namespace ExpressionToCodeTest
 {
+    /// <summary>
+    /// Repro for https://github.com/dotnet/runtime/issues/29673
+    /// </summary>
     public class ExpressionInterpretationBug
     {
         struct AStruct
@@ -21,13 +25,26 @@ namespace ExpressionToCodeTest
             => e;
 
         [Fact]
-        public void DotNetCoreIsNotBuggy()
+        public void MemberMemberBindingForStructsCompilesOk()
         {
             var expr_compiled = Expr(() => new SomethingMutable { AStructField = { AValue = 2 } }).Compile(false)();
             Assert.Equal(2, expr_compiled.AStructField.AValue);
-
+        }
+#if !NETFRAMEWORK
+        [Fact]
+        public void MemberMemberBindingForStructsInterpretsWrong_BUGBUG()
+        {
             var expr_interpreted = Expr(() => new SomethingMutable { AStructField = { AValue = 2 } }).Compile(true)();
-            Assert.Equal(2, expr_interpreted.AStructField.AValue);
+            //Assert.Equal(2, expr_interpreted.AStructField.AValue);  //this should hold, but instead:
+            Assert.Equal(0, expr_interpreted.AStructField.AValue); //this way I might notice when the bug gets fixed
+        }
+#endif
+
+        [Fact]
+        public void MemberMemberBindingForStructsFastCompilesOk()
+        {
+            var expr_compile_fast = Expr(() => new SomethingMutable { AStructField = { AValue = 2 } }).CompileFast()();
+            Assert.Equal(2, expr_compile_fast.AStructField.AValue);
         }
     }
 }
