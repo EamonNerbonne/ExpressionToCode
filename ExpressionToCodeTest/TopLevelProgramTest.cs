9d86cb73814d7a2e903dd696ab9c43ba8dbdf941
Eamon Nerbonne <eamon@nerbonne.org>
2021-02-21 20:34:24 +0100

    Actually test that the expressions are correctly generated in the top-level-program.

--- ExpressionToCodeTest/TopLevelProgramTest.cs
+++ ExpressionToCodeTest/TopLevelProgramTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using TopLevelProgramExample;
 using Xunit;
+using static TopLevelProgramExample.TopLevelProgramMarker;
 
 namespace ExpressionToCodeTest
 {
@@ -9,8 +10,14 @@ namespace ExpressionToCodeTest
         [Fact]
         public void CanRunTopLevelProgram()
         {
+            LambdaInsideLocalFunction = LambdaToMyVar = LambdaInsideNestedClassMethod = null;
+
             var topLevelProgram = (Action<string[]>)Delegate.CreateDelegate(typeof(Action<string[]>), typeof(TopLevelProgramMarker).Assembly.EntryPoint);
             topLevelProgram(new[] { "test" });
+
+            Assert.Equal("() => myVariable", LambdaToMyVar);
+            Assert.Equal("() => InnerClass.StaticInt + InstanceInt", LambdaInsideNestedClassMethod);
+            Assert.Equal("() => inLocalFunction + myVariable.Length - arg - withImplicitType.A.Length * 27", LambdaInsideLocalFunction);
         }
     }
 }
