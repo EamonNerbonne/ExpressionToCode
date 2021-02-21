35d68d7c0bd3ab1272ae22f65dd5264f2e7bf5b9
Eamon Nerbonne <eamon@nerbonne.org>
2021-02-21 19:34:15 +0100

    Add a proof-of-concept test

---
+++ ExpressionToCodeTest/TopLevelProgramTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TopLevelProgramExample;
+using Xunit;
+
+namespace ExpressionToCodeTest
+{
+    public class TopLevelProgramTest
+    {
+        [Fact]
+        public void CanRunTopLevelProgram()
+        {
+            var topLevelProgram = (Action<string[]>)Delegate.CreateDelegate(typeof(Action<string[]>), typeof(TopLevelProgramMarker).Assembly.EntryPoint);
+            topLevelProgram(new []{"test"});
+        }
+    }
+}
