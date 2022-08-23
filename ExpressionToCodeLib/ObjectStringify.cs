1c0f9a6930d6e142edf66f0303bc3d13233c9fe0
Sam Mansfield <sam.mansfield@saluscyber.com>
2022-08-23 11:02:31 +0100

    Add configuration options

--- ExpressionToCodeLib/ObjectStringify.cs
+++ ExpressionToCodeLib/ObjectStringify.cs
@@ -8,5 +8,7 @@ namespace ExpressionToCodeLib
     {
         public static readonly IObjectStringifier Default = new ObjectStringifyImpl();
         public static readonly IObjectStringifier WithFullTypeNames = new ObjectStringifyImpl(true);
+        public static readonly IObjectStringifier WithoutLiteralStrings = new ObjectStringifyImpl(false, false);
+        public static readonly IObjectStringifier WithFullTypeNamesWithoutLiteralStrings = new ObjectStringifyImpl(true, false);
     }
 }
