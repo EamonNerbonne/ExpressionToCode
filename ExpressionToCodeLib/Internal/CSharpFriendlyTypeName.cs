a42c6923ce870ba74cc595c7078a3094e6d7c137
Eamon Nerbonne <eamon@nerbonne.org>
2022-09-02 12:58:47 +0200

    use switch

--- ExpressionToCodeLib/Internal/CSharpFriendlyTypeName.cs
+++ ExpressionToCodeLib/Internal/CSharpFriendlyTypeName.cs
@@ -51,45 +51,26 @@ struct CSharpFriendlyTypeName
     }
 
     static string? AliasNameOrNull(Type type)
-    {
-        if (type == typeof(bool)) {
-            return "bool";
-        } else if (type == typeof(byte)) {
-            return "byte";
-        } else if (type == typeof(sbyte)) {
-            return "sbyte";
-        } else if (type == typeof(char)) {
-            return "char";
-        } else if (type == typeof(decimal)) {
-            return "decimal";
-        } else if (type == typeof(double)) {
-            return "double";
-        } else if (type == typeof(float)) {
-            return "float";
-        } else if (type == typeof(int)) {
-            return "int";
-        } else if (type == typeof(uint)) {
-            return "uint";
-        } else if (type == typeof(long)) {
-            return "long";
-        } else if (type == typeof(ulong)) {
-            return "ulong";
-        } else if (type == typeof(object)) {
-            return "object";
-        } else if (type == typeof(short)) {
-            return "short";
-        } else if (type == typeof(ushort)) {
-            return "ushort";
-        } else if (type == typeof(string)) {
-            return "string";
-        } else if (type == typeof(void)) {
-            return "void";
-        } else if (type.IsGenericParameter) {
-            return type.Name;
-        } else {
-            return null;
-        }
-    }
+        => type switch {
+            _ when type == typeof(bool) => "bool",
+            _ when type == typeof(byte) => "byte",
+            _ when type == typeof(sbyte) => "sbyte",
+            _ when type == typeof(char) => "char",
+            _ when type == typeof(decimal) => "decimal",
+            _ when type == typeof(double) => "double",
+            _ when type == typeof(float) => "float",
+            _ when type == typeof(int) => "int",
+            _ when type == typeof(uint) => "uint",
+            _ when type == typeof(long) => "long",
+            _ when type == typeof(ulong) => "ulong",
+            _ when type == typeof(object) => "object",
+            _ when type == typeof(short) => "short",
+            _ when type == typeof(ushort) => "ushort",
+            _ when type == typeof(string) => "string",
+            _ when type == typeof(void) => "void",
+            { IsGenericParameter: true } => type.Name,
+            _ => null,
+        };
 
     string? NullableTypeNameOrNull(TypeInfo typeInfo)
         => typeInfo.IsGenericType && !typeInfo.IsGenericTypeDefinition && typeInfo.GetGenericTypeDefinition() == typeof(Nullable<>) ? GetTypeName(typeInfo.GetGenericArguments().Single()) + "?" : null;
